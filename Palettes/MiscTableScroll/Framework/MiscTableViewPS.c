a430202ea8f7ddedb7c100e134ec324a97d3f073
C.W. Betts <computers57@hotmail.com>
2018-11-29 15:32:12 -0700

    Begin modernizing to work with Leopard/Mac OS X ppc.

---
+++ Palettes/MiscTableScroll/Framework/MiscTableViewPS.c
@@ -0,0 +1,19 @@
+//
+//  MiscTableViewPS.c
+//  MiscTableScroll
+//
+//  Created by C.W. Betts on 11/6/18.
+//  Copyright © 2018 C.W. Betts. All rights reserved.
+//
+
+#include "MiscTableViewPS.h"
+
+void MISC_TV_initps(void)
+{
+	printf("%s: stub!", __FUNCTION__);
+}
+
+void MISC_TV_dashedrects(float const* dr, int count)
+{
+	printf("%s, rects(%p) count(%d): stub!", __FUNCTION__, dr, count);
+}
